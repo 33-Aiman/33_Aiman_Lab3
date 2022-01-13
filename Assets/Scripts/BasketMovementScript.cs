6fadfa870c7ce0fdb48b5b87afda583a3a60b2bd
33_AimanEzrafil <phoenexus2107@gmail.com>
2022-01-13 10:08:48 +0800

    Updated Basketmovementscript(kindof)
    Movement Restricted but basket can move over the screen on the right a little. Left is ok. Still need to find solution to fix it.

--- Assets/Scripts/BasketMovementScript.cs
+++ Assets/Scripts/BasketMovementScript.cs
@@ -20,13 +20,19 @@ public class BasketMovementScript : MonoBehaviour
 
       transform.position = transform.position + new Vector3(horizontalInput * speed * Time.deltaTime, 0, 0);
 
-        
-       
+        var pos = Camera.main.WorldToViewportPoint(transform.position);
+        pos.x = Mathf.Clamp(pos.x, 0.1f, 0.93f);
+        pos.y = Mathf.Clamp(pos.y,0.1f, -7f);
+        transform.position = Camera.main.ViewportToWorldPoint(pos);
+
+
+
+
 
     }
 
 
 
-    
+
 
 }
